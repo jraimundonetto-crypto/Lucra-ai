4e72128d97e7837e9974097ea7ffaf24e8ef16c0
jraimundonetto-crypto <jraimundonetto@gmail.com>
2026-02-28 19:28:23 -0300

    Delete Nova pasta directory

--- Nova pasta/backend/src/config/openaiConfig.js
+++
@@ -1,9 +0,0 @@
-const { OpenAI } = require("openai");
-require("dotenv").config();
-
-// Inicialização profissional da OpenAI
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-module.exports = openai;
